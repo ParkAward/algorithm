22464692c727e11290180c5563039f22bfb497f7
ParkAward <parksangjun1363@gmail.com>
2022-03-23 22:36:01 +0900

    taxi geometry: https://www.acmicpc.net/problem/11021 Complete

---
+++ backjun/11021.java
@@ -0,0 +1,25 @@
+//taxi geometry: https://www.acmicpc.net/problem/11021 Complete
+
+import java.io.*;
+
+public class 11021 {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            String s = br.readLine();
+            for (int i = 0; i < Integer.valueOf(s); i++) {
+                String k = br.readLine();
+                String arr[] = k.split(" ");
+                int x = Integer.valueOf(arr[0]);
+                int y = Integer.valueOf(arr[1]);
+                bw.write("Case #"+(i+1)+": "+(x + y) + "\n");
+
+            }
+            bw.flush();
+        }catch (IOException e){
+
+        }
+
+    }
+}
