1d2692e8e3139ebbd1578510213597fb07d260b0
ParkAward <parksangjun1363@gmail.com>
2022-03-30 22:37:04 +0900

    I'll be the women's president: https://www.acmicpc.net/problem/11654 Complete

---
+++ backjun/11654.java
@@ -0,0 +1,19 @@
+//I'll be the women's president: https://www.acmicpc.net/problem/11654 Complete
+
+import java.io.*;
+
+public class 11654 {
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            String tmp = br.readLine();
+
+            bw.write((int)tmp.charAt(0)+"\n");
+            bw.flush();
+
+        } catch (IOException e) {
+        }
+    }
+
+}
