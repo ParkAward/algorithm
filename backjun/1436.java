0241dc285d74179c8d8adaf889389f3b4dc69979
ParkAward <parksangjun1363@gmail.com>
2022-04-07 22:38:17 +0900

    film director shome: https://www.acmicpc.net/problem/1436 Complete

---
+++ backjun/1436.java
@@ -0,0 +1,8 @@
+//film director shome: https://www.acmicpc.net/problem/1436 Complete
+class 1436{
+    public static void main(String[] args) {
+      int c=new java.util.Scanner(System.in).nextInt(),i=666,k=0;
+       while (k<c-1) if((++i+"").contains("666"))k++;
+        System.out.println(i);
+    }
+}
