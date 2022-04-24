2bf9058c07c855189c4691b2728266853966118b
ParkAward <parksangjun1363@gmail.com>
2022-04-24 22:40:47 +0900

    plain backpack: https://www.acmicpc.net/problem/2741 Complete

---
+++ backjun/2741.java
@@ -0,0 +1,12 @@
+//plain backpack: https://www.acmicpc.net/problem/2741 Complete
+
+import java.util.Scanner;
+
+public class 2741 {
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int num = scan.nextInt();
+    for(int i=1;i<=num;i++)
+        System.out.println(i);
+    }
+}
