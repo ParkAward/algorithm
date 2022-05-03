a16d7872dacecee282511f8e03bef78836c78b21
ParkAward <parksangjun1363@gmail.com>
2022-05-03 22:42:06 +0900

    A+B - 7: https://www.acmicpc.net/problem/3053 Complete

---
+++ backjun/3053.java
@@ -0,0 +1,7 @@
+//A+B - 7: https://www.acmicpc.net/problem/3053 Complete
+
+class 3053 {
+    public static void main(String[] args) { int s = new java.util.Scanner(System.in).nextInt();
+        System.out.printf("%f\n%f",s*s*Math.PI,s*s*2.0);
+    }
+}
